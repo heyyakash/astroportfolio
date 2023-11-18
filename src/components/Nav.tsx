8a8d19beb7dd85e3f1185d7870eb0c5ebaba85bc
heyyakash <akashsharma2002@gmail.com>
2023-11-18 15:10:59 +0530

    added dark mode

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,21 +1,29 @@
-import { CiLight,CiDark  } from "react-icons/ci";
+import { useState } from "react";
+import { CiLight, CiDark } from "react-icons/ci";
 
-export default function Nav(){
-    return(
+export default function Nav() {
+    const [mode, setMode] = useState<"light" | "dark">("dark")
+    const changeMode = (mode: "light" | "dark") => {
+        if(mode==="light") document.documentElement.classList.remove("dark")
+        else document.documentElement.classList.add("dark")
+        setMode(mode)
+    }
+    return (
         <nav className="w-full h-[70px]">
-        <div className="container-screen w-full h-full flex items-center justify-between">
-            <p className="text-sec font-semibold">SAT 12:24</p>
-            <div className="flex gap-4 font-semibold">
-                <div className={`nav-links`}>
-                    <CiLight className = "text-xl" />
-                    <p>LIGHT</p>
-                </div>
-                <div className={`nav-links text-white`}>
-                    <CiDark className = "text-xl"/>
-                    <p>DARK</p>
+            <div className="container-screen w-full h-full flex items-center justify-between">
+                <p className="text-sec font-semibold">SAT 12:24</p>
+                
+                <div className="flex gap-4 font-semibold">
+                    <div onClick={() => changeMode("light")} className={`nav-links ${mode==="light"?"text-white":""}`}>
+                        <CiLight className="text-xl" />
+                        <p>LIGHT</p>
+                    </div>
+                    <div onClick={() => changeMode("dark")} className={`nav-links ${mode==="dark"?"text-white":""}`}>
+                        <CiDark className="text-xl" />
+                        <p>DARK</p>
+                    </div>
                 </div>
             </div>
-        </div>
         </nav>
     )
 }
