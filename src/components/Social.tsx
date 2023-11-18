5a2c500c6e293553176a34ea347d9a8e934a0cdd
heyyakash <akashsharma2002@gmail.com>
2023-11-18 14:54:01 +0530

    made font bold

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -3,10 +3,10 @@ const Intro = () => {
     return (
         <Section title="Social">
             <div className='text-sec'>
-                <p>Send an email {"->"} <a target='_blank' href = "mailto:akashsharma2002@gmail.com" className='text-white'>akashsharma2002@gmail.com</a></p>
-                <p>DM on X {"->"} <a target='_blank' href = "https://x.com/heyyakash" className='text-white'>x.com/heyyakash</a></p>
-                <p>DM on LinkedIn {"->"} <a target='_blank' href = "https://www.linkedin.com/in/akash-sharma-a3536519a/" className='text-white'>Akash Sharma</a></p>
-                <p>Follow on GitHub {"->"} <a target='_blank' href = "https://github.com/heyyakash" className='text-white'>github.com/heyyakash</a></p>
+                <p>Send an email {"->"} <a target='_blank' href = "mailto:akashsharma2002@gmail.com" className='text-white font-semibold'>akashsharma2002@gmail.com</a></p>
+                <p>DM on X {"->"} <a target='_blank' href = "https://x.com/heyyakash" className='text-white font-semibold'>x.com/heyyakash</a></p>
+                <p>DM on LinkedIn {"->"} <a target='_blank' href = "https://www.linkedin.com/in/akash-sharma-a3536519a/" className='text-white font-semibold'>Akash Sharma</a></p>
+                <p>Follow on GitHub {"->"} <a target='_blank' href = "https://github.com/heyyakash" className='text-white font-semibold'>github.com/heyyakash</a></p>
                 
         
             </div>
