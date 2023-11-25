5c7999979c955bc419887ab69aea85067ddd9cbc
heyyakash <akashsharma2002@gmail.com>
2023-11-25 19:39:14 +0530

    fixed hobbies

--- src/components/Drawings.tsx
+++ src/components/Drawings.tsx
@@ -2,7 +2,7 @@ import drawings from "../data/drawings.json"
 
 const Drawings = () => {
   return (
-    <div className="grid  pt-2 grid-cols-1 md:grid-cols-3 grid-rows-auto auto-rows-[200px] gap-2">
+    <div className="grid  pt-2 grid-cols-1 md:grid-cols-3 grid-rows-auto md:auto-rows-[200px] gap-2">
         {drawings.map((x,i)=>{
             return(
                 <a href={x.loc} key = {i} target="_blank" className={`${x.attrib} rounded-md`}><img src={x.loc} className="w-full h-full rounded-md" alt="drawing" /></a>
